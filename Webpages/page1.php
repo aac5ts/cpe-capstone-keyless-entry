9094fc264fe7c6949c5035288fd8cd8810ec366d
Aditi Chaudhry <ac8wv@virginia.edu>
2014-10-15 15:11:42 -0400

    added password protection

--- Webpages/page1.php
+++ Webpages/page1.php
@@ -10,7 +10,9 @@ if(!isset($_SESSION["login"]))
 	}
 	 ?>
 
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" 
+
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <!-- DW6 -->
 <head>
@@ -30,6 +32,36 @@ if(!isset($_SESSION["login"]))
     <h1>Welcome</h1><br>
 
        <p><center>Welcome to our Capstone Project. Select your option below </center>
+
+
+
+<UL id="example_tree">
+	<LI><span>Turn LED ON/OFF</span>
+	
+		<UL>
+			<LI><span><a href="https://agent.electricimp.com/YNDvfcWn8MYH?
+
+led=1">ON</a></span></LI>
+			<LI><span><a href="https://agent.electricimp.com/YNDvfcWn8MYH?
+
+led=0">OFF</a></span></LI>	
+		</UL>
+	</LI>
+	<LI><span>Check Lock Status</span></LI>
+	<LI><span>Lock Door</span></LI>
+	<LI><span>Unlock Door</span></LI>
+</UL>
+
+<script src="http://code.jquery.com/jquery-1.10.1.min.js"></script>
+<script type="text/javascript">
+$(function(){
+$('#example_tree').find('SPAN').click(function(e){
+	$(this).parent().find('UL').toggle();
+});
+});
+</script>
+
+
         
 <br />   
 <P align="center">
--- Webpages/signup.php
+++ Webpages/signup.php
@@ -25,10 +25,94 @@ if(!isset($_SESSION["login"]))
   <link rel='stylesheet' href='http://codepen.io/assets/libs/fullpage/jquery-ui.css'>
 
     <link rel="stylesheet" href="css/style.css" media="screen" type="text/css" />
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
 
 </head>
 <body>
-
+	<style>
+input#pwd {
+        width:180px; 
+        padding:3px;
+        color: #000;
+        float:left;
+        margin-right:10px;
+}
+#pwd_strength_wrap {
+        border: 1px solid #D5CEC8;
+        display: none;
+        float: left;
+        padding: 10px;
+        position: relative;
+        width: 320px;
+}
+#pwd_strength_wrap:before, #pwd_strength_wrap:after {
+        content: ' ';
+        height: 0;
+        position: absolute;
+        width: 0;
+        border: 10px solid transparent; /* arrow size */
+}
+#pwd_strength_wrap:before {
+    border-bottom: 7px solid rgba(0, 0, 0, 0);
+    border-right: 7px solid rgba(0, 0, 0, 0.1);
+    border-top: 7px solid rgba(0, 0, 0, 0);
+    content: "";
+    display: inline-block;
+    left: -18px;
+    position: absolute;
+    top: 10px;
+}
+#pwd_strength_wrap:after {
+        border-bottom: 6px solid rgba(0, 0, 0, 0);
+    border-right: 6px solid #fff;
+    border-top: 6px solid rgba(0, 0, 0, 0);
+    content: "";
+    display: inline-block;
+    left: -16px;
+    position: absolute;
+    top: 11px;
+}
+#pswd_info ul {
+        list-style-type: none;
+        margin: 5px 0 0;
+        padding: 0;
+}
+#pswd_info ul li {
+        background: url(icon_pwd_strength.png) no-repeat left 2px;
+        padding: 0 0 0 20px;
+}
+#pswd_info ul li.valid {
+        background-position: left -42px;
+        color: green;
+}
+#passwordStrength {
+    display: block;
+    height: 5px;
+    margin-bottom: 10px;
+    transition: all 0.4s ease;
+}
+.strength0 {
+    background: none; /* too short */
+    width: 0px;
+}
+.strength1 {
+    background: none repeat scroll 0 0 #FF4545;/* weak */
+    width: 25px;
+}
+.strength2 {
+    background: none repeat scroll 0 0 #FFC824;/* good */
+    width: 75px;
+}
+.strength3 {
+        background: none repeat scroll 0 0 #6699CC;/* strong */
+    width: 100px;
+}
+ 
+.strength4 {
+        background: none repeat scroll 0 0 #008000;/* best */
+    width: 150px;
+}
+</style>
   <div class="login-card">
     <h1>Register</h1><br>
 
@@ -37,7 +121,21 @@ if(!isset($_SESSION["login"]))
         <p><FORM NAME ="form1" METHOD ="POST" ACTION ="signup.php">
 
 Username: <INPUT TYPE = 'TEXT' Name ='username'  value="<?PHP print $uname;?>" maxlength="20"><br />
-Password: <INPUT TYPE = 'password' Name ='password'  value="<?PHP print $pword;?>" maxlength="16"><br />
+Password: <INPUT TYPE = 'password' Name ='password'  value="<?PHP print $pword;?>" maxlength="16"><br /> 
+
+ <div id="pwd_strength_wrap">
+                <div id="passwordDescription">Password not entered</div>
+                <div id="passwordStrength" class="strength0"></div>
+                <div id="pswd_info">
+                        <strong>Strong Password Tips:</strong>
+                        <ul>
+                                <li class="invalid" id="length">At least 6 characters</li>
+                                <li class="invalid" id="pnum">At least one number</li>
+                                <li class="invalid" id="capital">At least one lowercase &amp; one uppercase letter</li>
+                                <li class="invalid" id="spchar">At least one special character</li>
+                        </ul>
+                </div><!-- END pswd_info -->
+        </div><!-- END pwd_strength_wrap -->
 
 
 
